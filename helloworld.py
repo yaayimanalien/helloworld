b6a2b3754d649af11b6323fe95868eb23982e8e8
yaayimanalien <mr.reinis5@gmail.com>
2019-06-11 22:33:19 +0300

    oof i broke it *_*

--- helloworld.py
+++ helloworld.py
@@ -1,23 +1,25 @@
 print('Hello, World!')
-key = ("hello123")
-name = True
+key = True
 username = True
 
 
 def welcome():
-    global name
-    if name != True:
-        print("Welcome, " + name + "!")
+    global username
+    
+    if username != True:
         fuction = str(input("Please select fuction: (login/register)"))
-        if fuction == "login":
-            login()
-        else:
-            if fuction == "register":
-                register()
+    if fuction == "login":
+        login()
     else:
-        name = str(input("Please enter your name: "))
+        if fuction == "register":
+             register()
+    # else:
+        username = str(input("Please enter your username: "))
         welcome()
-
+       
+    else:
+        print("Welcome, " + username + "!")
+    
 
 def login():
     global key
@@ -26,7 +28,7 @@ def login():
         print("Login successful.")
     else:
         print("Login unsuccessful. Please try again.")
-        login()
+        welcome()
 
 def register():
     global username
@@ -36,4 +38,3 @@ def register():
     welcome()
 
 welcome()
-raw_input()
