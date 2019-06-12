bd361def2664612423488d12f01a5db1f2b76325
yaayimanalien <mr.reinis5@gmail.com>
2019-06-12 15:50:46 +0300

    Fixed all issues

--- helloworld.py
+++ helloworld.py
@@ -1,34 +1,40 @@
 print('Hello, World!')
-key = True
-username = True
-
+key = ""
+username = ""
+username_login = ""
+password_login = ""
 
 def welcome():
     global username
-    
-    if username != True:
-        fuction = str(input("Please select fuction: (login/register)"))
+    print("Welcome, " + username + "!")
+
+def function():
+    fuction = str(input("Please select fuction: (login/register)"))
     if fuction == "login":
         login()
     else:
         if fuction == "register":
-             register()
-    # else:
-        username = str(input("Please enter your username: "))
-        welcome()
-       
-    else:
-        print("Welcome, " + username + "!")
-    
+            register()
+
 
 def login():
     global key
-    password = str(input("Please enter your password: "))
-    if password == key:
+    global username
+    global username_login
+    global password_login
+    username_login = str(input("Please enter your username: "))
+    password_login = str(input("Please enter your password: "))
+    login2()
+
+
+def login2():
+    if (username_login == username and password_login == key):
         print("Login successful.")
+
     else:
         print("Login unsuccessful. Please try again.")
-        welcome()
+        function()
+
 
 def register():
     global username
@@ -36,5 +42,10 @@ def register():
     username = str(input("Please enter your username: "))
     key = str(input("Please enter your password: "))
     welcome()
+    function()
 
-welcome()
+if username != "":
+    welcome()
+    function()
+else:
+    function()
